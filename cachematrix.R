d7633715dc18ab06acfb404e2ae7d7da5ef60e11
SP10000 <35839187+SP10000@users.noreply.github.com>
2018-03-25 11:33:10 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+makeCacheMatrix <- function(x = matrix()) { ## Define that the input x is a matrix
+  inv <- NULL
+  set <- function(y){
+    x <<- y ## Update variable x in the containing environment
+    inv <<- NULL ## Update variable inv in the containing environment
+  }
+  get <- function() x
+  setInverse <- function() inv <<- solve(x) ## Calculate the inverse of x, if it is not existing yet
+  getInverse <- function() inv ## Get inverse value in case the inverse is already calculated and stored
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse() ## Search whether there exists already a value for x in the cached memory
+  if(!is.null(inv)){ ## If there is cached data for x, retrieve the data
+    message("getting the cached data")
+    return(inv) ## Show the retrieved data
+      }
+  matrix <- x$get ## In case the data is not existing yet, calculate the inverse of the matrix
+  inv <- solve(matrix, ...)
+  x$setInverse(inv)
+  inv
+  }
